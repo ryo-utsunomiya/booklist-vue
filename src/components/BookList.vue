453f1c68340a775c449d4ec4bcd7155bbf1f9784
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-09-19 00:12:21 +0900

    Initial frontend implementation

---
+++ src/components/BookList.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+    <li v-for="book in books">
+      {{ book.title }} ({{ book.rate }})
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: 'bookList',
+    data() {
+      return {
+        books: [
+          {
+            title: 'C Programming Language',
+            rate: 1,
+          },
+          {
+            title: 'Learning Vue.js 2',
+            rate: 0,
+          },
+        ],
+      };
+    },
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Hello from '@/components/Hello';
+import BookList from '@/components/BookList';
 
 Vue.use(Router);
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello,
+      name: 'BookList',
+      component: BookList,
     },
   ],
 });
